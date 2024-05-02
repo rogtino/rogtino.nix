85ff716311d187cc7371c7c66cb559cc2526fbba
rogtino <rogtino@proton.me>
2024-05-02 15:34:59 +0800

    feat(waybar): add topbar

--- config/hypr/scripts/toggle.sh
+++ config/hypr/scripts/toggle.sh
@@ -7,7 +7,6 @@ if [ "$HYPRGAMEMODE" = 1 ]; then
         keyword decoration:blur:enabled 0;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
-        keyword group:groupbar:height 13;\
         keyword general:border_size 0;\
         keyword decoration:rounding 0"
 	notify-send "focusing"
@@ -19,7 +18,6 @@ else
         keyword decoration:blur:enabled 1;\
         keyword general:gaps_in 5;\
         keyword general:gaps_out 15;\
-        keyword group:groupbar:height 8;\
         keyword general:border_size 2;\
         keyword decoration:rounding 2"
 	notify-send "animating"
