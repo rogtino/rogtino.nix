e379efecc5365f5047183a1f3bc7709000ccbca7
rogtino <rogtino@proton.me>
2024-06-30 14:21:38 +0800

    fix(wsl): move sddm outside of core

--- system/core.nix
+++ system/core.nix
@@ -60,14 +60,6 @@
     libinput.enable = true;
   };
 
-  services.displayManager.sddm = {
-    package = pkgs.kdePackages.sddm;
-    catppuccin.enable = true;
-    catppuccin.fontSize = "18";
-    catppuccin.font = "Martian Mono";
-    catppuccin.background = "/home/gus/Pictures/wallpapers/190e4e36f72345799583c21201d06b2a.png";
-    enable = true;
-  };
   # Enable sound.
   sound.enable = true;
 
--- system/service/default.nix
+++ system/service/default.nix
@@ -1,8 +1,16 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./polkit-agent.nix
     ./guix.nix
     ./power.nix
   ];
   services.gvfs.enable = true;
+  services.displayManager.sddm = {
+    package = pkgs.kdePackages.sddm;
+    catppuccin.enable = true;
+    catppuccin.fontSize = "18";
+    catppuccin.font = "Martian Mono";
+    catppuccin.background = "/home/gus/Pictures/wallpapers/190e4e36f72345799583c21201d06b2a.png";
+    enable = true;
+  };
 }
